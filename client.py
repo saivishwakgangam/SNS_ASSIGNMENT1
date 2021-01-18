048e94c01912cf0b7dae692ca045ec9b4cba952e
Sai Vishwak Gangam <70585835+saivishwakgangam@users.noreply.github.com>
2021-01-19 01:19:21 +0530

    Implemented p2p messaging(non encrypted)

--- client.py
+++ client.py
@@ -1,8 +1,41 @@
 import socket
-clientsocket=socket.socket()
+import sys
+import os
+from _thread import start_new_thread
+def parse(message):
+    list=message.split()
+    return list[0]
 ipaddress='127.0.0.1'
 portno=1234
-print('waiting for response from server')
+def receivingdata(connection,port):
+    received=connection.recv(2048)
+    print(received.decode('utf-8'))
+    connection.close()
+    
+def clientlistenthread():
+    listeningsocket=socket.socket()
+    try:
+        listeningsocket.bind((ipaddress,int(sys.argv[1])))
+    except socket.error as e:
+        print(str(e))
+    print('client is listening on port no'+str(sys.argv[1]))
+    listeningsocket.listen(5)
+    while True:
+        client, address =listeningsocket.accept()
+        start_new_thread(receivingdata,(client,address[1]))
+def send(received,messagesent):
+    newsocket=socket.socket()
+    list=recevied.split()
+    try:
+        newsocket.connect((ipaddress,int(list[1])))
+    except socket.error as e:
+        print(str(e))
+    newsocket.send(str.encode(messagesent))
+    
+start_new_thread(clientlistenthread,())
+####
+clientsocket=socket.socket()
+#connection with server is starting
 try:
     clientsocket.connect((ipaddress,portno))
 except socket.error as e:
@@ -11,10 +44,22 @@ res=clientsocket.recv(1024)
 print(res.decode('utf-8'))
 while True:
     message=input()
+    initial=parse(message)
+    if(initial=='signup'):
+        message+=" "
+        message+=sys.argv[1]
+    elif(initial=='send'):
+        list=message.split(' ',2)
+        sendingmessage=list[2]
+        message=list[0]+' '+list[1]
+
     clientsocket.send(str.encode(message))
     res=clientsocket.recv(1024)
-    print(res.decode('utf-8'))
+    recevied=res.decode('utf-8')
+    initial=parse(recevied)
+    if(initial=='portno'):
+        send(recevied,sendingmessage)
+    else:
+        print(recevied)
 clientsocket.close()
-
-
 
