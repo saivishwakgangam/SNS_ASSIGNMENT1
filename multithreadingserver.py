253b98fef2ce70d99296a117d68c522d7c0fa993
Sai Vishwak Gangam <70585835+saivishwakgangam@users.noreply.github.com>
2021-01-19 01:17:51 +0530

    Implemented send message p2p (non encrypted)

--- multithreadingserver.py
+++ multithreadingserver.py
@@ -10,9 +10,10 @@ def create_user(message,portno):
         if(list[1]==clientinfo[i]['rollno']):
             response='roll number already present please enter another one'
             return response
-    clientinfo.append({'rollno':list[1],'username':list[2],'password':list[3],'login':False,'port':portno})
+    clientinfo.append({'rollno':list[1],'username':list[2],'password':list[3],'login':False,'port':list[4]})
     response='you have signed up succesfully now please sign in'
     return response
+
 def login(message,portno):
     list=message.split()
     length=len(clientinfo)
@@ -26,7 +27,16 @@ def login(message,portno):
     return response
 
 
-    
+def sendmessage(message):
+    list=message.split()
+    length=len(clientinfo)
+    for i in range(length):
+        if(list[1]==clientinfo[i]['rollno'] and clientinfo[i]['login']==True):
+            response='portno '+clientinfo[i]['port']
+            return response
+    response='member not present in the network or he is not login!!'
+    return response
+     
 def parse(message):
     list=message.split()
     return list[0]
@@ -41,6 +51,7 @@ except socket.error as e:
     print(str(e))
 print('server is listening')
 Serversocket.listen(5)
+
 def multithreadedclient(connection,portno):
     connection.send(str.encode("hello i am there!! and your port no is"+str(portno)))
     while True:
@@ -51,7 +62,9 @@ def multithreadedclient(connection,portno):
             resp=create_user(response,portno)
         elif(initial=='signin'):
             resp=login(response,portno)
-            
+        elif(initial=='send'):
+            resp=sendmessage(response)
+
             
         if not receiveddata:
             break
