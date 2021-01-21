2fa9d8a6c51a4513523bb93caca82b02debf1236
yochan2020202001 <70582656+yochan2020202001@users.noreply.github.com>
2021-01-21 22:54:01 +0530

    Updated server
    Added Group functionalities. Working model

--- multithreadingserver.py
+++ multithreadingserver.py
@@ -1,9 +1,17 @@
 import socket
 import os
 from _thread import start_new_thread
+
+
 clientinfo=[]
-groupinfo=[]
+
+groupinfo={}
+'''
+It is a dictinary where group name is key.
+Value is another dictionary with keys "participants" - a list of usernames, "admin" - user who created , "nonce" - a group secret key
+'''
 def create_user(message,portno):
+    print(message)
     list=message.split()
     length=len(clientinfo)
     for i in range(length):
@@ -11,6 +19,7 @@ def create_user(message,portno):
             response='roll number already present please enter another one'
             return response
     clientinfo.append({'rollno':list[1],'username':list[2],'password':list[3],'login':False,'port':list[4]})
+    print(clientinfo)
     response='you have signed up succesfully now please sign in'
     return response
 
@@ -26,6 +35,60 @@ def login(message,portno):
     response='invalid credentials please check'
     return response
 
+def joinGroup(message):
+    print(message)
+    command_list = message.split()
+    print(command_list)
+    group_request_list = list(command_list[2:-1])
+    username = command_list[-1]
+    print(group_request_list)
+    response =""
+    for group in group_request_list:
+        if group in groupinfo:
+            groupinfo[group]["participants"].append(command_list[-1])
+            response += "you are added to group " + group +"\n"
+        else: 
+            groupinfo[group] = {"participants" : [username], "admin" : username, "nonce" : os.urandom(24)}
+            response += "Created Group " + group + "Admin : "+ admin + "\n"
+    print(groupinfo)
+    return response
+
+def createGroup(message):
+    command_list = message.split()
+    print(command_list)
+    group_request_list = list(command_list[2:-1])
+    username = command_list[-1]
+    print(group_request_list)
+    response =""
+    for group in group_request_list:
+        if group in groupinfo:
+            response += "Group " + group + " Already exists. Join the group by Join Group command"+"\n"
+        else:
+            groupinfo[group] = {"participants" : [username], "admin" : username, "nonce" : os.urandom(24)}
+            response += "Created Group " + group + " ||  Admin : "+ username + "\n"
+    print(groupinfo)
+    return response
+
+def listGroups():
+    response = ""
+    for group in groupinfo:
+        response += group+ " participant count: "+ str(len(groupinfo[group]["participants"]))+"\n"
+    return response
+
+def deleteGroup(message):
+    print(message)
+    command_list = message.split()
+    print(command_list)
+    username = command_list[-1]
+    response =""
+    for g in command_list[2:-1]:
+        if groupinfo[g]["admin"] == username:
+            groupinfo.pop(g)
+            response += " Group " + g + " deleted successfully\n"
+        else:
+            response += "You do not have authority to delete Group. Only admin of the group can delete the group"+ g+ "\n"
+    print(groupinfo)
+    return response
 
 def sendmessage(message):
     list=message.split()
@@ -58,14 +121,26 @@ def multithreadedclient(connection,portno):
         receiveddata=connection.recv(2048)
         response=receiveddata.decode('utf-8')
         initial=parse(response)
+
         if(initial=='signup'):
             resp=create_user(response,portno)
         elif(initial=='signin'):
             resp=login(response,portno)
+
         elif(initial=='send'):
             resp=sendmessage(response)
 
-            
+
+        elif(initial == 'create'):
+            resp = createGroup(response)
+        elif(initial == 'delete'):
+            resp = deleteGroup(response)
+
+        elif(initial == 'join'):
+            resp = joinGroup(response)
+        elif(initial == 'list'):
+            resp = listGroups()
+
         if not receiveddata:
             break
         connection.sendall(str.encode(resp))
@@ -77,6 +152,3 @@ while True:
     count+=1
     print('Clients Connected :' + str(count))
 Serversocket.close()
-
-
-        
